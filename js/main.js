db7b29ed96c0af60936703970425b9485a54d0d4
Theodor Vararu <theo@tvararu.ro>
2014-05-03 20:21:58 +0300

    Trying out a temporary placeholder idea.

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+  var browsers = ['-ms-', '-webkit-', '-moz-', '-o-', ''];
+  var $wrapper = $('body');
+  var $logo = $('#logo');
+
+  $wrapper.mouseenter(function() {
+    for (var i = 0, len = browsers.length; i < len; i++) {
+      $logo.css(browsers[i] + 'transition', 'none');
+    }
+  });
+
+  $wrapper.mousemove(function(event) {
+    cx = Math.ceil($logo.width() / 2.0);
+    cy = Math.ceil($logo.height() / 2.0);
+
+    var off = $logo.offset();
+    dx = event.pageX - cx - off.left;
+    dy = event.pageY - cy - off.top;
+
+    tiltx = (dy / cy);
+    tilty = -(dx / cx);
+    radius = Math.sqrt(Math.pow(tiltx, 2) + Math.pow(tilty, 2));
+    degree = (radius * 20);
+
+    for (var i = 0, len = browsers.length; i < len; i++) {
+      $logo.css(
+        browsers[i] + 'transform',
+        'rotate3d(' + tiltx + ', ' + tilty + ', 0, ' + degree + 'deg)'
+      );
+    }
+  });
+
+  $wrapper.mouseleave(function() {
+    for (var i = 0, len = browsers.length; i < len; i++) {
+      $logo.css(browsers[i] + 'transition', 'all 0.2s');
+      $logo.css(browsers[i] + 'transform', 'none');
+    }
+  });
+});
