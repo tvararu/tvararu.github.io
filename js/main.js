3917e8d31df12e498f326df67a528d3e33fd14b3
Theodor Vararu <theo@tvararu.ro>
2014-05-08 21:04:23 +0300

    Set up bower, add Modernizr, make glasses mobile-friendly.

--- js/main.js
+++ js/main.js
@@ -3,12 +3,6 @@ $(document).ready(function() {
   var $wrapper = $('.container');
   var $logo = $('#logo');
 
-  $wrapper.mouseenter(function() {
-    for (var i = 0, len = browsers.length; i < len; i++) {
-      $logo.css(browsers[i] + 'transition', 'none');
-    }
-  });
-
   $wrapper.mousemove(function(event) {
     cx = Math.ceil($logo.width() / 2.0);
     cy = Math.ceil($logo.height() / 2.0);
@@ -17,8 +11,8 @@ $(document).ready(function() {
     dx = event.pageX - cx - off.left;
     dy = event.pageY - cy - off.top;
 
-    tiltx = (dy / cy);
-    tilty = -(dx / cx);
+    tiltx = -(dy / cy);
+    tilty = (dx / cx);
     radius = Math.sqrt(Math.pow(tiltx, 2) + Math.pow(tilty, 2));
     degree = (radius * 20);
 
@@ -30,10 +24,22 @@ $(document).ready(function() {
     }
   });
 
-  $wrapper.mouseleave(function() {
+  if (Modernizr.touch) {
     for (var i = 0, len = browsers.length; i < len; i++) {
       $logo.css(browsers[i] + 'transition', 'all 0.2s');
-      $logo.css(browsers[i] + 'transform', 'none');
     }
-  });
+  } else {
+    $wrapper.mouseenter(function() {
+      for (var i = 0, len = browsers.length; i < len; i++) {
+        $logo.css(browsers[i] + 'transition', 'none');
+      }
+    });
+
+    $wrapper.mouseleave(function() {
+      for (var i = 0, len = browsers.length; i < len; i++) {
+        $logo.css(browsers[i] + 'transition', 'all 0.2s');
+        $logo.css(browsers[i] + 'transform', 'none');
+      }
+    });
+  }
 });
