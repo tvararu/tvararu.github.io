13e3e8076122a8d9a63e8427dd711952e6586f0b
Theodor Văraru <theo@vararu.org>
2014-06-28 22:26:29 +0300

    Add gulp and browser-sync.

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+gulp.task('default', ['serve']);
+
+gulp.task('serve', function() {
+  browserSync.init(null, {
+    server: {
+      baseDir: './'
+    },
+    notify: false
+  });
+
+  gulp.watch([
+    './*.html',
+    './css/**/*.css',
+    './js/**/*.js'
+  ], browserSync.reload);
+});
