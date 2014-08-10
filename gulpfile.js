74cee428a92281010db7051ac877cd35db4179bb
Theodor Văraru <theo@vararu.org>
2014-08-10 22:28:22 +0300

    Restructure folders.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,20 @@
+'use strict';
+
+var path = require('path');
+
 var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+
+var wiredep = require('wiredep').stream;
 var browserSync = require('browser-sync');
 
-gulp.task('default', ['serve']);
+var paths = {
+  app: './app',
+  tmp: './tmp',
+  dist: './dist'
+};
+
+gulp.task('default', ['build']);
 
 gulp.task('serve', function() {
   browserSync.init(null, {
@@ -19,3 +32,10 @@ gulp.task('serve', function() {
     browserSync.reload({ once: true });
   });
 });
+
+gulp.task('build', function () {
+  return gulp.src(path.join(paths.app, 'index.html'))
+    .pipe(wiredep())
+    .pipe(gulp.dest(paths.tmp));
+});
+
