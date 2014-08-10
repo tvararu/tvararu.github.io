2970ddab2f557452002fdd1fce7b26471f9b6375
Theodor Văraru <theo@vararu.org>
2014-08-11 00:55:38 +0300

    Remove other loadCSS function.

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,18 @@
+var loadCSS = function (path) {
+  var cb = function () {
+    var l = document.createElement('link'); l.rel = 'stylesheet';
+    l.href = path;
+    var h = document.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
+  };
+  var raf = requestAnimationFrame || mozRequestAnimationFrame ||
+      webkitRequestAnimationFrame || msRequestAnimationFrame;
+  if (raf) {
+    raf(cb);
+  } else {
+    window.addEventListener('load', cb);
+  }
+};
+
 $(document).ready(function() {
   loadCSS('http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600&subset=latin,latin-ext');
   loadCSS('css/main.css');
